01376a73e1d460bf83f590187e8f313739c0f835
Zachary <160025727+ZackIsInTheBox@users.noreply.github.com>
2024-03-05 13:07:05 +0000

    Update DesktopLauncher.java

--- desktop/src/com/heshus18/game/DesktopLauncher.java
+++ desktop/src/com/heshus18/game/DesktopLauncher.java
@@ -8,7 +8,7 @@ import com.heshus18.game.MyGameMain;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setTitle("Droop");
+		config.setTitle("Heslington Hustle");
 		config.setWindowedMode(800, 480);
 		config.useVsync(true);
 		config.setForegroundFPS(60);
