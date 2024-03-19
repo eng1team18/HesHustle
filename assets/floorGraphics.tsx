4da8494725e698fc2ad189ab017b70c1deb6b178
owenkil <160025222+owenkil@users.noreply.github.com>
2024-03-19 15:16:57 +0000

    Map v4
    Map now uses assets from a pack on itch io, this version of the map also is testing hitboxes for potential trees.

---
+++ assets/floorGraphics.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="floorGraphics" tilewidth="32" tileheight="32" tilecount="350" columns="10">
+ <image source="Outdoor.png" width="320" height="1120"/>
+</tileset>
