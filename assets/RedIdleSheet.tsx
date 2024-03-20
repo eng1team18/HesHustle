3358bdb15dbbc64edb625b40aedfcbd17ae2c9f3
owenkil <160025222+owenkil@users.noreply.github.com>
2024-03-20 20:18:01 +0000

    Map v6
    Added the new NPC sprites and (hopefully) completed the map.

---
+++ assets/RedIdleSheet.tsx
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="RedIdleSheet" tilewidth="32" tileheight="32" tilecount="16" columns="8">
+ <image source="RedIdleSheet.png" width="256" height="64"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="150"/>
+   <frame tileid="2" duration="150"/>
+   <frame tileid="4" duration="150"/>
+   <frame tileid="6" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="150"/>
+   <frame tileid="3" duration="150"/>
+   <frame tileid="5" duration="150"/>
+   <frame tileid="7" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="8" duration="150"/>
+   <frame tileid="10" duration="150"/>
+   <frame tileid="12" duration="150"/>
+   <frame tileid="14" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <animation>
+   <frame tileid="9" duration="150"/>
+   <frame tileid="11" duration="150"/>
+   <frame tileid="13" duration="150"/>
+   <frame tileid="15" duration="150"/>
+  </animation>
+ </tile>
+</tileset>
